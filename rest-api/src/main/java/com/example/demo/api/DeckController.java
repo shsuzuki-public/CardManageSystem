6582d4ef76f3fe959e28605b5ba9fda8b109067c
shsuzuki <shsuzuki.public@gmail.com>
2018-12-11 01:34:27 +0900

    DB直呼び出し⇒APIからデータ取得に変更

--- rest-api/src/main/java/com/example/demo/api/DeckController.java
+++ rest-api/src/main/java/com/example/demo/api/DeckController.java
@@ -7,23 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.domain.ClanM;
 import com.example.demo.domain.DeckM;
-import com.example.demo.repository.DeckRepository;
 import com.example.demo.service.ClanService;
 import com.example.demo.service.DeckService;
 
 @Controller
 public class DeckController {
 	
+	RestTemplate restTemplate = new RestTemplate();
+	
 	@Autowired
 	ClanService clanService;
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/api/clans", method=RequestMethod.GET)
 	public String index(Model model){
-		List<ClanM> clans = clanService.getData();
+		List<ClanM> clans = restTemplate.getForObject("http://localhost:8090/clans", List.class);
 		model.addAttribute("clans",clans);
 		return "html/top";
 	}
@@ -31,11 +33,18 @@ public class DeckController {
 	@Autowired
 	DeckService deckService;
 	
-	@RequestMapping(value="/api/items", method=RequestMethod.GET)
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/api/decks", method=RequestMethod.GET)
 	public String getItems(Model model) {
+		//RestTemplate restTemplate = new RestTemplate();
+		List<DeckM> decks = restTemplate.getForObject("http://localhost:8090/decks", List.class);
+		model.addAttribute("decks", decks);
 		
+		return "html/list";
+		/*
 		List<DeckM> customers = deckService.getData();
 		model.addAttribute("customers",customers);
 		return "html/list";
+		*/
 	}
 }
